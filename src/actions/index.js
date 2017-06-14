d4b9435bdc165520d317f09ce76a39dccc3d9ee4
Ben Gimbel <BenGimbel@Bens-MacBook-Pro.local>
2017-06-14 09:08:01 -0500

    changed weather to current forecast

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const API_KEY = 'ffb77ac13145c86461fe1b75018e732b';
-//const ROOT_URL = `http://api.openweathermap.org/data/2.5/weather?appid=${API_KEY}`
-const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/weather?appid=${API_KEY}`;
+// const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city) {

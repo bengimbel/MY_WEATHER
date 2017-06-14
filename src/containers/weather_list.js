6376f7e005b60b5f943993ac6a5a5c95c5044c94
Ben Gimbel <BenGimbel@Bens-MacBook-Pro.local>
2017-06-14 16:23:49 -0500

    changed to <tr><td> cause google map was not re-rendering

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -16,33 +16,53 @@ class WeatherList extends Component {
 
 
   return(
-    <div>
-    <div className='googlemap'><GoogleMap lon={lon} lat={lat} /></div>
-      <div>
-        <h1>{cityName}</h1>
-
-      </div>
-      <div>
-        <h3>{minTemp}</h3>
-        <h3>{temp}</h3>
-        <h3>{maxTemp}</h3>
-      </div>
-      <div>
-        <h2 key={status}>{status}</h2>
-        <h2 key={description}>{description}</h2>
-      </div>
-    </div>
+    // <div>
+    // <div className='googlemap'><GoogleMap lon={lon} lat={lat} /></div>
+    //   <div>
+    //     <h1>{cityName}</h1>
+    //     <h1>{lat}</h1>
+    //     <h1>{lon}</h1>
+    //   </div>
+    //   <div>
+    //     <h3>{minTemp}</h3>
+    //     <h3>{temp}</h3>
+    //     <h3>{maxTemp}</h3>
+    //   </div>
+    //   <div>
+    //     <h2 key={status}> {status} </h2>
+    //     <h2 key={description}> {description} </h2>
+    //   </div>
+    // </div>
+    <tr key={cityName}>
+  <td><GoogleMap lon={lon} lat={lat} /></td>
+  <td><h1>{lat}</h1> <h1>{lon}</h1></td>
+  <td><h3>{minTemp}</h3><h3>{temp}</h3><h3>{maxTemp}</h3></td>
+  <td><h2 key={status}> {status} </h2><h2 key={description}> {description} </h2></td>
+</tr>
     );
   }
 
   render() {
     return (
-        <div>
-          <h1>City</h1>
-            <div>
-              {this.props.weather.map(this.renderWeather)}
-            </div>
-        </div>
+        // <div>
+        //   <h1>City</h1>
+        //     <div>
+        //       {this.props.weather.map(this.renderWeather)}
+        //     </div>
+        // </div>
+        <table className="table table-hover">
+  <thead>
+    <tr>
+      <th>City</th>
+      <th>Temperature (K) </th>
+      <th>Pressure (hpa)</th>
+      <th>Humidity (%)</th>
+    </tr>
+  </thead>
+  <tbody>
+    {this.props.weather.map(this.renderWeather)}
+  </tbody>
+</table>
     )
   }
 }
